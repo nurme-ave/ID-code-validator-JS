d606e1f7e23b3332ee14525647fd52b50cc2e24b
Ave Nurme <nurme.ave@gmail.com>
2022-03-20 21:20:28 +0200

    Improve appearance - mobile view

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 /* Hamburger Menu */
-const navBarToggleButton = document.querySelector('.nav-bar-toggle-button')
-const navMenu = document.querySelector('.nav-menu')
-const navMenuItems = document.querySelectorAll('.nav-menu-item')
+const navBarToggleButton = document.querySelector('.nav-bar-toggle-button');
+const navMenu = document.querySelector('.nav-menu');
+const navMenuItems = document.querySelectorAll('.nav-menu-item');
 
 navBarToggleButton.addEventListener('click', () => {
-  navMenu.classList.toggle('show')
-})
+  navMenu.classList.toggle('show');
+  navBarToggleButton.classList.toggle('rotate-lines');
+});
 
 /* Hide menu when clicked on the nav item */
 function clickOnNavItem() {
-  navMenu.classList.remove('show')
+  navMenu.classList.remove('show');
+  navBarToggleButton.classList.remove('rotate-lines');
 }
-navMenuItems.forEach((item) => item.addEventListener('click', clickOnNavItem))
-
+navMenuItems.forEach((item) => item.addEventListener('click', clickOnNavItem));
 
 /* User Input & Display Results */
-// const userInput = document.getElementById('user-input');
-// const displayResult = document.querySelector('.display-result');
+const userInput = document.getElementById('user-input');
+const displayResult = document.querySelector('.display-result');
 
-// userInput.addEventListener('keydown', (e) => {
-//   if (e.key === 'Enter') {
-//     e.preventDefault();
-//     if (!userInput.value) {
-//       displayResult.textContent = '';
-//     } else {
-//       displayResult.textContent = 'Your ID-code is valid.';
-//     }
-//   }
-// });
+userInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    e.preventDefault();
+    if (!userInput.value) {
+      displayResult.textContent = '';
+    } else {
+      displayResult.textContent = 'Your ID-code is valid.';
+    }
+  }
+});

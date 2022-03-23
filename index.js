4da2028ab39e4c774a5fe6f6079b10653efc381e
Ave Nurme <nurme.ave@gmail.com>
2022-03-23 23:54:24 +0200

    Add validation logic in JS

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function clickOnNavItem() {
 }
 navMenuItems.forEach((item) => item.addEventListener('click', clickOnNavItem));
 
-/* User Input & Display Results */
+/* User Input */
 const userInput = document.getElementById('user-input');
 const displayResult = document.querySelector('.display-result');
 
@@ -25,7 +25,129 @@ userInput.addEventListener('keydown', (e) => {
     if (!userInput.value) {
       displayResult.textContent = '';
     } else {
-      displayResult.textContent = '>>> Result here';
+      isValidIdCode(userInput.value);
     }
   }
 });
+
+/* Validate & Display Results */
+function isValidIdCode(idCode) {
+  const length = isOfCorrectLength(idCode);
+  const genderNumber = isOfCorrectGenderNumber(idCode);
+  const validYear = isValidYear(idCode);
+  const validMonthNumber = isValidMonthNumber(idCode);
+  const validDate = isValidDate(idCode);
+  const validBornOrder = isValidBornOrder(idCode);
+  const validChecksum = isValidChecksum(idCode);
+
+  console.log(length);
+  console.log(genderNumber);
+  console.log(validYear);
+  console.log(validMonthNumber);
+  console.log(validDate);
+  console.log(validBornOrder);
+  console.log(validChecksum);
+
+  // console.log(convertToFullYear(idCode));
+  // console.log(isLeapYear(idCode));
+
+  if (
+    length &&
+    genderNumber &&
+    validYear &&
+    validMonthNumber &&
+    validDate &&
+    validBornOrder &&
+    validChecksum
+  ) {
+    console.log('All Good');
+    displayResult.textContent = 'Valid ID-code';
+  } else {
+    console.log('Somethings not quite right');
+    displayResult.textContent = 'Invalid ID-code';
+  }
+}
+
+function isOfCorrectLength(idCode) {
+  return idCode.length === 11;
+}
+
+function isOfCorrectGenderNumber(idCode) {
+  const genderNumbers = [1, 2, 3, 4, 5, 6];
+  return genderNumbers.includes(+idCode[0]);
+}
+
+function isValidYear(idCode) {
+  return 0 <= +idCode.slice(1, 3) && +idCode.slice(1, 3) <= 99;
+}
+
+function isValidMonthNumber(idCode) {
+  return 0 < +idCode.slice(3, 5) && +idCode.slice(3, 5) <= 12;
+}
+
+function convertToFullYear(idCode) {
+  let fullYear;
+  if (+idCode[0] === 3 || +idCode[0] === 4) {
+    fullYear = `19${idCode.slice(1, 3)}`;
+  } else if (+idCode[0] === 5 || +idCode[0] === 6) {
+    fullYear = `20${idCode.slice(1, 3)}`;
+  }
+  return +fullYear;
+}
+
+function isLeapYear(idCode) {
+  fullYear = convertToFullYear(idCode);
+  return fullYear % 400 == 0 || (fullYear % 4 == 0 && fullYear % 100 != 0);
+}
+
+function isValidDate(idCode) {
+  const monthNumber = +idCode.slice(3, 5);
+  const date = +idCode.slice(5, 7);
+  const monthsWith31Days = [1, 3, 5, 7, 8, 10, 12];
+  const monthsWith30Days = [4, 6, 9, 11];
+
+  if (monthNumber === 2 && date <= 28) {
+    return true;
+  } else if (monthNumber === 2 && isLeapYear(idCode) && date <= 29) {
+    return true;
+  } else if (monthsWith31Days.includes(monthNumber) && date <= 31) {
+    return true;
+  } else if (monthsWith30Days.includes(monthNumber) && date <= 30) {
+    return true;
+  }
+  return false;
+}
+
+function isValidBornOrder(idCode) {
+  return 0 <= +idCode.slice(7, 10) && +idCode.slice(7, 10) <= 999;
+}
+
+function isValidChecksum(idCode) {
+  const weightkArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1];
+  const weightkArray2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3];
+  const splitIdCode = idCode.slice(0, 10).split('');
+
+  let arrayOfNumbers = splitIdCode.map((str) => {
+    return +str;
+  });
+
+  let sum = 0;
+  for (let i = 0; i < weightkArray1.length; i++) {
+    sum += weightkArray1[i] * arrayOfNumbers[i];
+  }
+
+  let remainder = sum % 11;
+  if (remainder >= 10) {
+    sum = 0;
+    for (let i = 0; i < weightkArray2.length; i++) {
+      sum += weightkArray2[i] * arrayOfNumbers[i];
+    }
+
+    if (remainder >= 10) {
+      return true;
+    }
+  } else if (remainder === +idCode[10]) {
+    return true;
+  }
+  return false;
+}

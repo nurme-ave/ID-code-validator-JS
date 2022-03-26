90224db62472639125c5dc8558f82e5691018241
Ave Nurme <nurme.ave@gmail.com>
2022-03-27 00:51:57 +0200

    Work on code readibility, refactor & test

--- index.js
+++ index.js
@@ -15,11 +15,10 @@ function clickOnNavItem() {
 }
 navMenuItems.forEach((item) => item.addEventListener('click', clickOnNavItem));
 
-/* User Input */
+/* Validation */
 const userInput = document.getElementById('user-input');
 const displayResult = document.querySelector('.dp');
 const submitButton = document.querySelector('.submit-button');
-
 
 userInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
@@ -29,19 +28,17 @@ userInput.addEventListener('keydown', (e) => {
 
 userInput.addEventListener('click', () => {
   displayResult.textContent = '';
-})
+});
 
 submitButton.addEventListener('click', (e) => {
   e.preventDefault();
   if (!userInput.value) {
-      displayResult.textContent = 'Please insert your ID-code';
-    } else {
-      isOfCorrectLength(userInput.value);
-    }
+    displayResult.textContent = 'Please insert your ID-code';
+  } else {
+    isOfCorrectLength(userInput.value);
+  }
 });
 
-
-/* Validate & Display Results */
 function isOfCorrectLength(idCode) {
   if (idCode.length === 11) {
     isValidIdCode(idCode);
@@ -51,69 +48,67 @@ function isOfCorrectLength(idCode) {
 }
 
 function isValidIdCode(idCode) {
-    const genderNumber = isOfCorrectGenderNumber(idCode);
-    const validYear = isValidYear(idCode);
-    const validMonthNumber = isValidMonthNumber(idCode);
-    const validDate = isValidDate(idCode);
-    const validBornOrder = isValidBornOrder(idCode);
-    const validChecksum = isValidChecksum(idCode);
-    
-    
-    console.log(genderNumber);
-    console.log(validYear);
-    console.log(validMonthNumber);
-    console.log(validDate);
-    console.log(validBornOrder);
-    console.log(validChecksum);
-  
-    // console.log(convertToFullYear(idCode));
-    // console.log(isLeapYear(idCode));
-  
-    if (
-      genderNumber &&
-      validYear &&
-      validMonthNumber &&
-      validDate &&
-      validBornOrder &&
-      validChecksum
-    ) {
-      console.log('All Good');
-      displayResult.textContent += `${userInput.value} is valid`;
-    } else {
-      console.log('Somethings not quite right');
-      displayResult.textContent += `${userInput.value} is not valid`;
-    }
-    userInput.value = '';
+  const genderNumber = isOfCorrectGenderNumber(idCode);
+  const validYear = isValidYear(idCode);
+  const validMonth = isValidMonth(idCode);
+  const validDate = isValidDate(idCode);
+  const validBornOrder = isValidBornOrder(idCode);
+  const validChecksum = isValidChecksum(idCode);
+
+  console.log(genderNumber);
+  console.log(validYear);
+  console.log(validMonth);
+  console.log(validDate);
+  console.log(validBornOrder);
+  console.log(validChecksum);
+
+  // console.log(convertToFullYear(idCode));
+  // console.log(isLeapYear(idCode));
+
+  if (
+    genderNumber &&
+    validYear &&
+    validMonth &&
+    validDate &&
+    validBornOrder &&
+    validChecksum
+  ) {
+    console.log('All Good');
+    displayResult.textContent += `${userInput.value} is valid`;
+  } else {
+    console.log('Somethings not quite right');
+    displayResult.textContent += `${userInput.value} is not valid`;
+  }
+  userInput.value = '';
 }
 
-
-
 function isOfCorrectGenderNumber(idCode) {
+  const genderNumber = +idCode[0];
   const genderNumbers = [1, 2, 3, 4, 5, 6];
-  return genderNumbers.includes(+idCode[0]);
+  return genderNumbers.includes(genderNumber);
 }
 
 function isValidYear(idCode) {
-  return 0 <= +idCode.slice(1, 3) && +idCode.slice(1, 3) <= 99;
+  const birthYear = +idCode.slice(1, 3);
+  return 0 <= birthYear && birthYear <= 99;
 }
 
-function isValidMonthNumber(idCode) {
-  return 0 < +idCode.slice(3, 5) && +idCode.slice(3, 5) <= 12;
+function isValidMonth(idCode) {
+  const birthMonth = +idCode.slice(3, 5);
+  return 0 < birthMonth && birthMonth <= 12;
 }
-
-// function convertToFullYear(idCode) {
-//   let fullYear;
-//   if (+idCode[0] === 3 || +idCode[0] === 4) {
-//     fullYear = `19${idCode.slice(1, 3)}`;
-//   } else if (+idCode[0] === 5 || +idCode[0] === 6) {
-//     fullYear = `20${idCode.slice(1, 3)}`;
-//   }
-//   return +fullYear;
-// }
 
 function isLeapYear(idCode) {
-  let fullYear;
+  /*
+    In order to determine whether a year is leap year
+    we need the full year with 4 digits (e.g 1970).
+    If the first digit of the ID code is either 3 or 4,
+    this means the person was born in the 19th century.
+    If the first digit of the ID code is either 5 or 6,
+    this means the person was born in the 20th century.
+  */
 
+  let fullYear;
   if (+idCode[0] === 3 || +idCode[0] === 4) {
     fullYear = `19${idCode.slice(1, 3)}`;
   } else if (+idCode[0] === 5 || +idCode[0] === 6) {
@@ -124,29 +119,30 @@ function isLeapYear(idCode) {
 }
 
 function isValidDate(idCode) {
-  const monthNumber = +idCode.slice(3, 5);
-  const date = +idCode.slice(5, 7);
+  birthMonth = +idCode.slice(3, 5);
+  const birthDate = +idCode.slice(5, 7);
   const monthsWith31Days = [1, 3, 5, 7, 8, 10, 12];
   const monthsWith30Days = [4, 6, 9, 11];
 
-  if (monthNumber === 2 && date <= 28) {
+  if (birthMonth === 2 && birthDate <= 28) {
     return true;
-  } else if (monthNumber === 2 && isLeapYear(idCode) && date === 29) {
+  } else if (birthMonth === 2 && isLeapYear(idCode) && birthDate === 29) {
     return true;
-  } else if (monthsWith31Days.includes(monthNumber) && date <= 31) {
+  } else if (monthsWith31Days.includes(birthMonth) && birthDate <= 31) {
     return true;
-  } else if (monthsWith30Days.includes(monthNumber) && date <= 30) {
+  } else if (monthsWith30Days.includes(birthMonth) && birthDate <= 30) {
     return true;
   }
   return false;
 }
 
 function isValidBornOrder(idCode) {
-  return 0 <= +idCode.slice(7, 10) && +idCode.slice(7, 10) <= 999;
+  const bornOrder = +idCode.slice(7, 10);
+  return 0 <= bornOrder && bornOrder <= 999;
 }
 
 function isValidChecksum(idCode) {
-  let remainder, secondChRem, controlNum;
+  let remainder, controlNum;
   const weightkArray1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1];
   const weightkArray2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3];
   const splitIdCode = idCode.slice(0, 10).split('');
@@ -155,12 +151,12 @@ function isValidChecksum(idCode) {
 
   if (remainder >= 10) {
     remainder = mainCheck(splitIdCode, weightkArray2);
-    secondChRem = secondCheck(remainder);
-    return finalCheck(secondChRem, idCode);
+    controlNum = secondCheck(remainder);
+    // return finalCheck(controlNum, idCode);
   } else {
     controlNum = remainder;
-    return finalCheck(controlNum, idCode);
   }
+  return finalCheck(controlNum, idCode);
 }
 
 function mainCheck(code, weight) {
@@ -171,15 +167,13 @@ function mainCheck(code, weight) {
   return sum % 11;
 }
 
-function finalCheck(cNum, code) {
-  return cNum === +code[10];
+function finalCheck(num, code) {
+  return num === +code[10];
 }
 
 function secondCheck(rem) {
   if (rem >= 10) {
     rem = 0;
-    return rem;
-  } else {
-    return rem;
   }
+  return rem;
 }

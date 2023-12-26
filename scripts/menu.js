fd6a7d41dfa42e0a790c3f852bfa3d9c66a0f056
Ave Nurme <nurme.ave@gmail.com>
2023-12-26 15:39:08 +0200

    Add function to change year in the footer dynamically

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,14 @@
+/* Update the year in the footer dynamically */
+const footer = document.querySelector('footer')
+
+function updateYear() {
+  const currentYear = new Date().getFullYear()
+  const yearElement = footer.querySelector('#currentYear')
+  yearElement.textContent = currentYear
+}
+
+updateYear()
+
 /* Hamburger Menu */
 const navBarToggleButton = document.getElementById('nav-bar-toggle-button');
 const navMenu = document.getElementById('nav-menu');
